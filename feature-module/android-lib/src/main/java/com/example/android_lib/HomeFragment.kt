23cca4bce37cb3e4173e4be8d60dc2bcb0daad31
Volodymyr Lohachov <115993681+vlohachov@users.noreply.github.com>
2023-03-20 19:47:58 +0200

    Update HomeFragment.kt

--- feature-module/android-lib/src/main/java/com/example/android_lib/HomeFragment.kt
+++ feature-module/android-lib/src/main/java/com/example/android_lib/HomeFragment.kt
@@ -18,7 +18,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+    private var param1: Int? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,10 +48,10 @@ class HomeFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: Int, param2: String) =
             HomeFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
+                    putInt(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
