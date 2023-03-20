fe687ff0b214f1d168607e0f93a29f6e7da64e31
vlohachov <vlohachov-ecw@cricut.com>
2023-03-21 00:52:24 +0700

    Update HomeFragment.kt.

--- feature-module/android-lib/src/main/java/com/example/android_lib/HomeFragment.kt
+++ feature-module/android-lib/src/main/java/com/example/android_lib/HomeFragment.kt
@@ -24,7 +24,7 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
+            param1 = it.getInt(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
